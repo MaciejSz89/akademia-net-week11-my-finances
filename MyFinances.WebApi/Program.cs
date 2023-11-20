d8aefb145930ccd7774ea9a93990e6adb63fe7d9
Maciej Szafranski <maciej.szafranski1@gmail.com>
2023-11-20 20:55:01 +0100

    second commit

---
+++ MyFinances.WebApi/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MyFinances.WebApi.Models;
+using MyFinances.WebApi.Models.Domains;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory,
+        $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+});
+
+
+builder.Services.AddDbContext<MyFinancesContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("MyFinancesContext")));
+builder.Services.AddScoped<UnitOfWork, UnitOfWork>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
